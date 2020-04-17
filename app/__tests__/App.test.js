309815009d11f097aa89b085f59e9499fccb6194
unknown <nicoletobe2000@gmail.com>
2020-04-17 10:46:53 +0400

    added tests folder

---
+++ app/__tests__/App.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import App from '../App.js';
+
+/* ---------- APP --------- */
+
+
+describe('Main app component', () => {
+    it('should render with given state', () => {
+    });
+    it('should render 2 children elements', () => {
+    });
+});
+
+/* -------- BLOCK LIST --------- */
+
+describe('BlockList Component', () => {
+    it('should render with  children', () => {
+    });
+
+});
+
+/* -------- CLOCK --------- */
+
+describe('Clock Component', () => {
+    it('should render a date', () => {
+    });
+});
+
+/* -------- TODO --------- */
+
+describe('Todo Component', () => {
+    it('should contain no todos', () => {
+        
+    });
+
+    it('should render a todo element', () => {
+
+    });
+    it('should remove a todo element', () => {
+        
+    });
+
+});
+
+
+/* -------- SETTINGS --------- */
+
+/* -------- ADD BLOCK --------- */
