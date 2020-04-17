15857c4e7d69bf71c9dbd68bdcdcbb4c87b56448
unknown <nicoletobe2000@gmail.com>
2020-04-17 11:01:06 +0400

    added blocklist container component and block component + its presentation logic

---
+++ app/components/Block.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ClockBlock from './Blocks/ClockBlock';
+import WeatherBlock from './Blocks/WeatherBlock';
+import TodoBlock from './Blocks/TodoBlock';
+
+const Block = ({block}) =>{
+    const CurrentBlock;
+    switch(block.id){
+        case 1:
+            CurrentBlock = <ClockBlock/>;
+            break;
+        case 2:
+            CurrentBlock = <WeatherBlock/>;
+            break;
+        case 3:
+            CurrentBlock = <TodoBlock/>;
+            break;
+    }
+
+    return(
+            <div className= "block">
+            <CurrentBlock/> 
+            </div>
+    )
+}
+export default Block;
--- app/components/BlockList.js
+++ app/components/BlockList.js
@@ -1,1 +1,43 @@
 import React, {Component} from 'react';
+import Block from './Block';
+
+class BlockList extends Component {
+    constructor(props){
+        super(props);
+        this.state = { 
+            settingsShown: false,
+            blocks: [
+                 {
+                    id: 1,
+                    name: 'Clock',
+                    shown: false,
+                }, 
+                {
+                    id:2,
+                    name: 'Weather',
+                    shown: false,
+                }, 
+                {
+                    id:3,
+                    name: 'Todo List',
+                    shown: false,
+                },
+            ]
+        }
+    }
+
+    render(){
+        return (
+            <div className="blocklist">
+            {this.state.blocks.map(block=>{ if (block.shown) {
+               return <Block key={block.id} block={block}/>
+            } })
+            }
+            </div>
+        )
+    }
+    
+    
+}
+
+export default BlockList;
